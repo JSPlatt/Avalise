fe4fd37bda482b81a608b7e3fc4c012be262b17a
Jeffrey Platt <jsplatt@gmail.com>
2021-06-24 15:32:44 -0400

    routing started

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,7 @@ import { ShoppingCart } from '@material-ui/icons'
 import { mergeClasses } from '@material-ui/styles'
 import logo from '../../assets/avalise2.png'
 import useStyles from './styles'
+import { Link } from 'react-router-dom'
 
 const Navbar = ({ totalItems }) => {
     const classes = useStyles()
@@ -11,13 +12,13 @@ const Navbar = ({ totalItems }) => {
         <>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography variant='h6' className={classes.title} color='inherit'>
+                    <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
                         <img src={logo} alt="Avalise" height="25px" className={classes.image}/>
                         Avalise
                     </Typography>
                     <div className={classes.grow}/>
                     <div className={classes.button}>
-                        <IconButton aria-label="Show cart items" color="inherit">
+                        <IconButton component={Link} to='/cart' aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
