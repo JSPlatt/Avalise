59f0f22900b9788fc09e91b9640be5e9173b19e6
Jeffrey Platt <jsplatt@gmail.com>
2021-06-25 16:33:23 -0400

    basic address form setup

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { InputLabel, Select, MenuItem, Grid, Typography } from '@material-ui/core'
 import { useForm, FormProvider } from 'react-hook-form'
 import FormInput from './CustomTextField'
-
+import { commerce } from '../../lib/commerce'
 
 const AddressForm = () => {
+    const [shippingCountries, setShippingCountries] = useState([])
+    const [shippingCountry, setShippingCountry] = useState ('US')
+    const [shippingSubdivisions, setShippingSubdivisions] = useState ([])
+    const [shippingSubdivision, setShippingSubdivision] = useState ('')
+    const [shippingOptions, setShippingOptions] = useState([])
+    const [shippingOption, setShippingOption] = useState('')
+
     const methods = useForm()
+    const fetchShippingCountries = async (checkoutTokenId) => {
+        const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId)
+
+        setShippingCountries(countries)
+    }
 
     return (
         <>
@@ -14,6 +26,38 @@ const AddressForm = () => {
                 <form onSubmit=''>
                     <Grid container spacing = {3}>
                         <FormInput required name='firstName' label='First name'/>
+                        <FormInput required name='lastName' label='Last name'/>
+                        <FormInput required name='address' label='Address'/>
+                        <FormInput required name='email' label='Email'/>
+                        <FormInput required name='city' label='City'/>
+                        <FormInput required name='zip' label='ZIP / Postal code'/>
+                        <Grid type xs={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+                            <Select value='' fullWidth onChange=''>
+                                <MenuItem key='' value=''>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+
+                        </Grid>
+                        <Grid type xs={12} sm={6}>
+                            <InputLabel>Shipping Subdivision</InputLabel>
+                            <Select value='' fullWidth onChange=''>
+                                <MenuItem key='' value=''>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+
+                        </Grid>
+                        <Grid type xs={12} sm={6}>
+                            <InputLabel>Shipping Options</InputLabel>
+                            <Select value='' fullWidth onChange=''>
+                                <MenuItem key='' value=''>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+
+                        </Grid>
                     </Grid>
                 </form>
             </FormProvider>
