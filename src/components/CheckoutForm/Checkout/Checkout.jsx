84ae0ce64e99c0f6ba3719ac6d0bba971d584cc6
Jeffrey Platt <jsplatt@gmail.com>
2021-06-28 13:56:53 -0400

    working on checkout token

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core'
 import useStyles from './styles'
 import AddressForm from '../AddressForm'
 import PaymentForm from '../PaymentForm'
+import { commerce } from '../../../lib/commerce'
 
 
 const steps = ['Shipping address', 'Payment details']
 
-const Checkout = () => {
+const Checkout = ({ cart }) => {
     const[activeStep, setActiveStep] = useState(0)
+    const [chekoutToken, setCheckoutToken] = useState(null)
     const classes = useStyles()
     
+
+    useEffect(() => {
+        const generateToken = async () => {
+            try {
+                const token = await commerce.checkout.generateToken(cart.id, { type: 'cart' })
+
+                console.log(token)
+
+                setCheckoutToken(token)
+            } catch (error) {
+
+            }
+        }
+
+        generateToken()
+    },[])
+    
     const Confirmation = () => (
-        <div>Confirmation</div>
+        <div>
+            Confirmation
+        </div>
     )
 
     const Form = () => activeStep === 0 
