550877efc0a8a58a106e1cd40d6852bf40a9b074
Jeffrey Platt <jsplatt@gmail.com>
2021-07-01 11:07:50 -0400

    initial Stripe setup

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import { Typography,Button, Divider } from '@material-ui/core'
 import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js'
-import { LoadStripe } from '@stripe/stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 import Review from './Review'
 
 
-const PaymentForm = ({ checkoutToken }) => {
+const stripePromise = loadStripe(process.env.REACT_APP__STRIPE_PUBLIC_KEY)
+
+const PaymentForm = ({ checkoutToken, backStep }) => {
+
+    const handleSubmit = () => {
+
+    }
+
     return (
         <>
             <Review checkoutToken={checkoutToken}/>
+            <Divider />
+            <Typography variant='h6' gutterBottom style={{margin: '20px 0'}}>Payment method</Typography>
+            <Elements stripe={stripePromise}>
+                <ElementsConsumer>
+                    {(elements, stripe) => (
+                        <form onSubmit={(e) => handleSubmit}>
+                            <CardElement />
+                            <br /> <br />
+                            <div style={{display: 'flex', justifyContent:'space-between'}}>
+                                <Button variant='outlined' onClick={backStep}>Back</Button>
+                                <Button type='submit' variant='contained' disabled={!stripe} color='primary'>
+                                    Pay { checkoutToken.live.subtotal.formatted_with_symbol}
+                                </Button>
+                            </div>
+                        </form>
+                    )}
+                </ElementsConsumer>
+            </Elements>
         </>
     )
 }
